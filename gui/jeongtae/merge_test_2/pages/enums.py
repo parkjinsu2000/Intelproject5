eae1e8de5ec8e14a3747e1d79579f1ea4032b2fe
jungtae hwang <ghkdwjdxo99@naver.com>
2025-09-08 18:18:23 +0900

    feat: add auto-refresh for challenge video registration
    - Fixed signal connection for "Add New Challenge Video" button
    - Implemented video registration to resources/videos/ref with duplicate checks
    - Updated rank_video_list.txt automatically on new video registration
    - Added newChallengeVideoAdded signal in MainPage
    - Connected signal in MainWindow to refresh VideoSelectPage and RankPage lists

--- gui/jeongtae/merge_test_2/pages/enums.py
+++ gui/jeongtae/merge_test_2/pages/enums.py
@@ -4,6 +4,7 @@ class PageIndex(IntEnum):
     MAIN = 0
     RANK = 1
     VIDEO_SELECT = 2
+    ADD_NEW_CHALLENGE_VIDEO = 3
     # PoseScoreApp 같은 동적 페이지는 따로 관리 (매번 새로 생성)
 
 class ModeNumber(IntEnum):
