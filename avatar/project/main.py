627e8fca35b438b0b6f4f1df0120c2b05c8919c9
Juyeong Min <judy440@skuniv.ac.kr>
2025-09-11 10:07:06 +0900

    avatar 수정

--- avatar/project/main.py
+++ avatar/project/main.py
@@ -1,12 +1,5 @@
 # main.py
-from avatar_ui import run_app
-
-OPTIONS = {
-    1: ("dance_poses.json", "naruto_parts"),
-    2: ("dance_poses.json", "mannequin_parts"),
-    3: ("dance_poses.json", "naruto_parts_alt"),
-    4: ("dance_poses.json", "naruto_parts"),
-}
+from avatar_ui import run_app, DEFAULT_OPTIONS
 
 if __name__ == "__main__":
-    run_app(OPTIONS)
+    run_app(DEFAULT_OPTIONS) 
